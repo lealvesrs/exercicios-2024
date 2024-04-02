a2c77593bdabad6105073e940d8964e75b87f674
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-04-02 09:49:03 -0300

    feat: Spout to write xlsx

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,24 +1,24 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
+
 error_reporting(E_ERROR | E_PARSE);
 
-/**
- * Runner for the Webscrapping exercice.
- */
-class Main {
 
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
-  public static function run(): void {
+
+class Main
+{
+
+  public static function run(): void
+  {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+    $scrapper = new Scrapper();
+    $data = ($scrapper ->scrap($dom));
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+    $scrapper->writeToXlsx($data);
+
   }
 
 }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -4,6 +4,7 @@ namespace Chuva\Php\WebScrapping;
 
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 
 /**
  * Does the scrapping of a webpage.
@@ -20,43 +21,91 @@ class Scrapper
     $anchors = $dom->getElementsByTagName('a');
 
     foreach ($anchors as $anchor) {
+      if ($anchor->childElementCount > 0) {
+        //finding the title
+        $title = $anchor->firstElementChild->textContent;
 
-      //finding the title
-      $title = $anchor->firstElementChild->textContent;
+        //finding the name of all authors and their institutions
+        $spans = $anchor->getElementsByTagName('span');
 
-      //finding the name of all authors and their institutions
-      $spans = $anchor->getElementsByTagName('span');
+        $authors = [];
+        foreach ($spans as $span) {
+          $name = $span->textContent;
+          $institution = $span->getAttribute('title');
 
-      $authors = [];
-      foreach ($spans as $span) {
-        $name = $span->textContent;
-        $institution = $span->getAttribute('title');
+          $authors[] = new Person(
+            $name,
+            $institution
+          );
+        }
 
-        $authors[] = new Person(
-          $name,
-          $institution
+        //finding the type of the paper
+        $div_type = $anchor->lastElementChild;
+        $type = $div_type->firstElementChild->textContent;
+
+        //finding id paper
+        $div_volume = $div_type->lastElementChild;
+        $id_paper = $div_volume->textContent;
+
+        //creating the paper obj
+        $paper = new Paper(
+          $id_paper,
+          $title,
+          $type,
+          $authors
         );
+
+        $array[] = $paper;
       }
-
-      //finding the type of the paper
-      $div_type = $anchor->lastElementChild;
-      $type = $div_type->firstElementChild->textContent;
-
-      //finding id paper
-      $div_volume = $div_type->lastElementChild;
-      $id_paper = $div_volume->textContent;
-
-      //creating the paper obj
-      $paper = new Paper(
-        $id_paper,
-        $title,
-        $type,
-        $authors
-      );
-
-      $array[] = $paper;
     }
 
     return $array;
   }
+
+  public static function writeToXlsx(array $data): void
+  {
+      $writer = WriterEntityFactory::createXLSXWriter();
+      $writer->openToFile('./assets/results.xlsx');
+
+      //creating the header content
+      $header = ["ID", "Title", "Type"];
+
+      $maxAuthorsCount = max(array_map(function ($paper) {
+          return count($paper->authors);
+      }, $data));
+
+      $i = 0;
+      while ($i <= $maxAuthorsCount) {
+          $newHeaderCell[] = "Author $i";
+          $newHeaderCell[] = "Author $i institution";
+
+          $i++;
+      }
+
+      $header += $newHeaderCell;
+
+      //writing header
+      $row = WriterEntityFactory::createRowFromArray($header);
+      $writer->addRow($row);
+
+
+      //adding the paper rows
+      foreach ($data as $paper) {
+          $row = [
+              $paper->id,
+              $paper->title,
+              $paper->type,
+          ];
+
+          foreach ($paper->authors as $author) {
+              $row[] = $author->name;
+              $row[] = $author->institution;
+          }
+
+          $rowFromValues = WriterEntityFactory::createRowFromArray($row);
+          $writer->addRow($rowFromValues);
+      }
+      $writer->close();
+  }
+
 }
