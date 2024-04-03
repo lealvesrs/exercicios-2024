a97d77a17b487ca8a5190655aba6898d13cb02c4
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-04-03 11:45:13 -0300

    feat: creating the app bar

--- dart/lib/app/modules/calendar/view/calendar.dart
+++ dart/lib/app/modules/calendar/view/calendar.dart
@@ -26,10 +26,57 @@ class _CalendarState extends State<Calendar> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: fromCssColor("#456189"),
-        title: Text(
-          'Chuva ❤️ Flutter',
-          style: TextStyle(color: fromCssColor("white")),
+        toolbarHeight: 130,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 15.0),
+          child: Text(
+            'Chuva ❤️ Flutter',
+            style: TextStyle(color: fromCssColor("white")),
+          ),
         ),
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(0),
+            child: Column(
+              children: [
+                Text(
+                  "Programação",
+                  style:
+                      TextStyle(fontSize: 20, color: fromCssColor("lightgray")),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8),
+                  child: Container(
+                      height: 50,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(30)),
+                        color: fromCssColor("white"),
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Container(
+                              height: 40,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(30)),
+                                color: fromCssColor("#306dc3"),
+                              ),
+                              child: const Icon(Icons.calendar_month,
+                                  color: Colors.black),
+                            ),
+                          ),
+                          const Center(
+                              child: Text("Exibindo todas atividades")),
+                        ],
+                      )),
+                ),
+              ],
+            )),
       ),
       body: const Column(
         children: [
