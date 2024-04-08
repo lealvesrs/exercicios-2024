3429b25d76477062de793b20696871ca18b2e3f9
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-04-08 19:43:45 -0300

    feat: creating components to organize

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
