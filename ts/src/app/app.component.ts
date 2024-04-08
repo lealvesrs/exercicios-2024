8c41b632a69b6c4fafbec5e01de86436dd25d6b8
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-04-08 19:02:51 -0300

    feat: btn like

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP';
-  comments = [{ id: 1, comment:1, subject: "Assunto da pergunta aparece aqui", content: "Comecinho da pergunta aparece aqui neste relato inscreve-se no campo da anáise da dimensão e impacto de processo formativo situado impacto de processo formativo processo", author: "Letícia Alves de Oliveira" }];
+  topics = [{like:0, comment: 1, subject: "Assunto da pergunta aparece aqui", content: "Comecinho da pergunta aparece aqui neste relato inscreve-se no campo da anáise da dimensão e impacto de processo formativo situado impacto de processo formativo processo", author: "Letícia Alves de Oliveira" }];
 
   click() {
     var element = document.getElementById("section_hidden");
@@ -19,16 +19,37 @@ export class AppComponent {
   }
 
   createTopic() {
+    var feedback = document.getElementById("feedback");
     var subject = (<HTMLInputElement>document.getElementById("subject")).value;
     var content = (<HTMLInputElement>document.getElementById("content")).value;
-    if(content!="" && subject!=""){
-      this.comments.push({
-        id: 2,
-        comment:0,
+    if (content != "" && subject != "") {
+      this.topics.push({
+        like:0,
+        comment: 0,
         subject: subject,
         content: content,
         author: "Letícia Alves de Oliveira"
-      })
+      });
+
+      feedback?.classList.remove("d-none");
     }
+  }
+
+  openComment(topic: { comment: number; }) {
+    if(topic.comment>0){
+      var comment = document.getElementById("comment");
+    var status = comment?.classList.contains("d-block");
+    if (status) {
+        comment?.classList.remove("d-block");
+        comment?.classList.add("d-none")
+    }else{
+      comment?.classList.add("d-block")
+      comment?.classList.remove("d-none");
+    }
+    }
+  }
+
+  like(index:number){
+    this.topics[index].like++;
   }
 }
