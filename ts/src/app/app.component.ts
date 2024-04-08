dc413341bf5c5d8077b1c5c1b743a314aff3def7
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-04-08 18:12:01 -0300

    feat: add comment function

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,17 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP';
+  comments = [{ id: 1, comment:1, subject: "Assunto da pergunta aparece aqui", content: "Comecinho da pergunta aparece aqui neste relato inscreve-se no campo da anáise da dimensão e impacto de processo formativo situado impacto de processo formativo processo", author: "Letícia Alves de Oliveira" }];
 
-  click(){
+  click() {
     var element = document.getElementById("section_hidden");
     var form = document.getElementById("form");
-    if(element!=null && form!=null){
+    if (element != null && form != null) {
       element.style.display = "none";
       form.classList.remove("d-none")
     }
   }
 
-  createTopic(){
-
+  createTopic() {
+    var subject = (<HTMLInputElement>document.getElementById("subject")).value;
+    var content = (<HTMLInputElement>document.getElementById("content")).value;
+    if(content!="" && subject!=""){
+      this.comments.push({
+        id: 2,
+        comment:0,
+        subject: subject,
+        content: content,
+        author: "Letícia Alves de Oliveira"
+      })
+    }
   }
 }
