13489d0e892d95d904d008eb04b629d2319ee189
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-04-08 17:13:44 -0300

    feat: show form to create topic

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP';
+
+  click(){
+    var element = document.getElementById("section_hidden");
+    var form = document.getElementById("form");
+    if(element!=null && form!=null){
+      element.style.display = "none";
+      form.classList.remove("d-none")
+    }
+  }
+
+  createTopic(){
+
+  }
 }
