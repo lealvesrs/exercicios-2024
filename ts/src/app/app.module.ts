e1b0ba31a01538668ee3ed1cc6eec05977599567
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-04-08 22:00:36 -0300

    feat: add details component

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
